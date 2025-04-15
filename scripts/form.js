0540006e24a8813e9ace2f80087e32da387ea82a
altoledo27 <ana.toledo.zepahua@gmail.com>
2025-04-15 13:54:19 -0600

    form practice

---
+++ scripts/form.js
@@ -0,0 +1,74 @@
+// Get the current year dynamically
+const yearSpan = document.querySelector("#currentyear");
+const today = new Date();
+
+if (yearSpan) {
+  yearSpan.textContent = today.getFullYear();
+}
+const full = document.querySelector("#lastModified");
+
+if (full) {
+    full.innerHTML = `Last Modification: <span class="highlight">${document.lastModified}</span>`;
+}
+
+// get the number of visitors
+const visitsDisplay = document.querySelector(".visits");
+
+if (visitsDisplay) {
+    let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+    if (numVisits !== 0) {
+        visitsDisplay.textContent = numVisits;
+    } else {
+        visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+    }
+
+    numVisits++;
+    localStorage.setItem("numVisits-ls", numVisits);
+}
+
+//list of products
+
+const products = [
+    {
+      id: "fc-1888",
+      name: "Keyboard ⌨️",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "Mouse 🖱️",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "Microphone 🎤",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "Printer 🖨️",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "Trackball 🖲️",
+      averagerating: 5.0
+    }
+  ];
+
+
+
+const selectElement = document.querySelector("#productName");
+
+if (selectElement) {
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.textContent = product.name;
+    option.value = product.id;
+  
+    selectElement.appendChild(option);
+});
+} else {
+    console.error("❌ No se encontró el elemento con id #productName");
+}
