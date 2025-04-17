7d96a013c82b2c5d5ef1bdbec265cf5892cbbac3
altoledo27 <ana.toledo.zepahua@gmail.com>
2025-04-16 18:39:51 -0600

    corerections

--- scripts/form.js
+++ scripts/form.js
@@ -30,33 +30,32 @@ if (visitsDisplay) {
 //list of products
 
 const products = [
-    {
-      id: "fc-1888",
-      name: "Keyboard ⌨️",
-      averagerating: 4.5
-    },
-    {
-      id: "fc-2050",
-      name: "Mouse 🖱️",
-      averagerating: 4.7
-    },
-    {
-      id: "fs-1987",
-      name: "Microphone 🎤",
-      averagerating: 3.5
-    },
-    {
-      id: "ac-2000",
-      name: "Printer 🖨️",
-      averagerating: 3.9
-    },
-    {
-      id: "jj-1969",
-      name: "Trackball 🖲️",
-      averagerating: 5.0
-    }
-  ];
-
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
 
 
 const selectElement = document.querySelector("#productName");
